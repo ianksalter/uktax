cbe41eab0578656ec340ba7a3ae4a8179cfbf420
Ian Salter <ianksalter@users.noreply.github.com>
2019-10-30 10:12:53 +0100

    Package cleanups suc as dependencies.

--- R/personal-allowance.R
+++ R/personal-allowance.R
@@ -1,21 +1,22 @@
-library(R6)
-library(tidyverse)
-library(purrr)
+# library(R6)
+# library(tidyverse)
 
 #' The Personal Allowance Class
 #'
-#' This class describes the personal allowance that a person has in the UK tax system.
-#' In the UK the personal allowance is how much you can earn tax free. However the allowance is
-#' withdrawn at the given rate over the threshold.
-#' Note this is a simplification of the actual UK tax system. For example
-#' this class ignores the married couples allowance.
+#' This class describes the personal allowance that a person has in the UK
+#' tax system.In the UK the personal allowance is how much you can earn
+#' tax free. However the allowance is withdrawn at the given rate over the
+#' threshold. Note this is a simplification of the actual UK tax system.
+#' For example this class ignores the married couples allowance.
 #' @field allowance The amount of the allowance.
 #' @field threshold The income at which the allowance starts to be withdrawn.
 #' @field rate The rate at which the allowance is withdrawn.
-#' @method amount Determines how much personal alowance a person has dependant upon their income.
+#' @method amount Determines how much personal alowance a person has dependant
+#' upon their income.
 #' @section Methods:
 #' \describe{
-#' \item{\code{amount}}{Determines how much personal alowance a person has dependant upon their income.}
+#' \item{\code{amount}}{Determines how much personal alowance a person has
+#' dependant upon their income.}
 #' }
 #' @keywords uk income tax, personal allowance
 #' @export
@@ -26,45 +27,44 @@ library(purrr)
 #'   withdrawalRate = 0.5
 #' )
 #' personalAllowance2017$amount(101000)
-PersonalAllowance <-R6Class("PersonalAllowance",
+PersonalAllowance <- R6Class("PersonalAllowance",
                             public = list(
                               base = NULL,
                               threshold = NULL,
                               withdrawalRate = NULL,
-                              initialize = function (base = NA, threshold = NA, withdrawalRate = NA){
+                              initialize = function(base = NA,
+                                                    threshold = NA,
+                                                    withdrawalRate = NA) {
                                 self$base <- base
                                 self$threshold <- threshold
                                 self$withdrawalRate <- withdrawalRate
                               },
-                              amount = function(income){
+                              amount = function(income) {
                                 if (income < self$threshold)
                                   self$base
                                 else
-                                  max(0 , self$base - floor((income-self$threshold)*self$withdrawalRate))
+                                  max(0,
+                                      self$base -
+                                        floor(
+                                          (income - self$threshold) *
+                                            self$withdrawalRate))
                               }
                             )
 )
 
 # Data sourced from:
 # https://www.gov.uk/government/publications/rates-and-allowances-income-tax/income-tax-rates-and-allowances-current-and-past
-personalAllowanceData <-
+personal_allowance_data <-
   tribble(
     ~year, ~base, ~threshold, ~withdrawalRate,
-    #----/-----/------/---
-    2016,11000,100000,0.5,
-    2017,11500,100000,0.5,
-    2018,11850,100000,0.5,
-    2019,12500,100000,0.5
+    #----/------/-----------/----------------
+    2016, 11000, 100000, 0.5,
+    2017, 11500, 100000, 0.5,
+    2018, 11850, 100000, 0.5,
+    2019, 12500, 100000, 0.5
   )
 
-filter(personalAllowanceData, year == 2016)$base #Note potentially useful
-
-pmap(list(personalAllowanceData$base,
-          personalAllowanceData$threshold,
-          personalAllowanceData$withdrawalRate),
-     PersonalAllowance$new)
-
-personalAllowances <-
+personal_allowances <-
   tibble(
     year = personalAllowanceData$year,
     allowance = pmap(list(personalAllowanceData$base,
@@ -90,6 +90,3 @@ personalAllowances <-
 #   threshold = 100000,
 #   withdrawalRate = 0.5
 # )
-
-
-
--- tests/test-personal-allowance.R
+++ tests/test-personal-allowance.R
@@ -1,22 +1,24 @@
 # Unit tests for the PersonalAllowance class
 
-source("R/personal-allowance.r")
+load_all()
 
 library(testthat)
 
-context('Testing PersonalAllowance class')
+context("Testing PersonalAllowance class")
 
-test_that('PersonalAllowance$amount method produce expected values',{
+test_that("PersonalAllowance$amount method produce expected values", {
 
-  personalAllowance2017 <- filter(personalAllowances, year == 2017)$allowance[[1]]
-  expect_equal(personalAllowance2017$amount(100000),11500)
-  expect_equal(personalAllowance2017$amount(100002),11499)
-  expect_equal(personalAllowance2017$amount(100004),11498)
-  expect_equal(personalAllowance2017$amount(123000),0)
+  personal_allowance_2017 <-
+    filter(personal_allowances, year == 2017)$allowance[[1]]
+  expect_equal(personal_allowance_2017$amount(100000), 11500)
+  expect_equal(personal_allowance_2017$amount(100002), 11499)
+  expect_equal(personal_allowance_2017$amount(100004), 11498)
+  expect_equal(personal_allowance_2017$amount(123000), 0)
 
-  personalAllowance2016 <- filter(personalAllowances, year == 2016)$allowance[[1]]
-  expect_equal(personalAllowance2016$amount(100000),11000)
-  expect_equal(personalAllowance2016$amount(100002),10999)
-  expect_equal(personalAllowance2016$amount(100004),10998)
-  expect_equal(personalAllowance2016$amount(123000),0)
+  personal_allowance_2016 <-
+    filter(personal_allowances, year == 2016)$allowance[[1]]
+  expect_equal(personal_allowance_2016$amount(100000), 11000)
+  expect_equal(personal_allowance_2016$amount(100002), 10999)
+  expect_equal(personal_allowance_2016$amount(100004), 10998)
+  expect_equal(personal_allowance_2016$amount(123000), 0)
 })
